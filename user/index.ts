771831beccfceecf32852e05e476cf20d74feb12
taha <jokerdz380@gmail.com>
2026-03-10 10:11:00 +0000

    config routes

--- user/index.ts
+++ user/index.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { login, signup } from "./user_auth";
 import express from "express";
+const app = express();
 const router = Router();
+app.use(express.json());
 router.post("/signup", signup);
 router.post("/login", login);
-const app = express();
-app.use(express.json());
+router.get('/', (req, res) => {
+    console.log('hello world ')
+    res.json({ msg: "hello worl from nginx " })
+})
 app.use("/auth", router);
-app.listen(3001 , ()=>{
+app.get('/', (req, res) => {
+    console.log('hello from user ')
+    res.status(200).json({ msg: 'hi from user' })
+})
+app.listen(3001, "0.0.0.0", () => {
     console.log('user')
 })
 export default router;
