b66ea9042f8dbb0c7ffa33ce07babf3b624e0e12
taha <jokerdz380@gmail.com>
2026-03-16 23:46:12 +0000

    setup ratelimiter

--- checkout/index.ts
+++ checkout/index.ts
@@ -1,1 +1,10 @@
-console.log('taga')
+import { Router } from "express";
+import express from "express";
+const app = express();
+app.get('/', (req, res) => {
+    console.log('hello from checkout ')
+    res.status(200).json({ msg: 'hi from checkout' })
+})
+app.listen(3002, "0.0.0.0", () => {
+    console.log('checkout')
+})
