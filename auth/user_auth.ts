34c0fe5507dbea626ad2dddfba607f9ea2f679c4
taha <jokerdz380@gmail.com>
2026-03-19 14:19:24 +0000

    auth test done

--- auth/user_auth.ts
+++ auth/user_auth.ts
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import pool from './db';
-
 import { Request, Response } from "express";
 
 export const signup = async (req: Request, res: Response) => {
@@ -29,7 +28,7 @@ export const signup = async (req: Request, res: Response) => {
       user: result.rows[0],
     });
   } catch (error) {
-    res.status(500).json({ error: "Internal server error", err:error });
+    res.status(500).json({ error: "Internal server error", err: error });
   }
 };
 
@@ -53,38 +52,42 @@ export const login = async (req: Request, res: Response) => {
     if (!validPassword) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-
+    console.log(`${process.env.JWT_SECRET}`)
     const token = jwt.sign(
       {
         userId: user.id,
         email: user.email,
       },
-      process.env.JWT_SECRET as string,
+      'process.env.JWT_SECRET' as string,
       {
         expiresIn: "1h",
       }
     );
-
     res.json({
       message: "Login success",
       token,
     });
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error", errr: error, tkn: `${process.env.JWT_SECRET}` });
   }
 };
-export const verifyToken = (req: Request, res: Response, next: Function) => {
+export const verifyToken = (req: Request, res: Response) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-      return res.status(401).json({ message: "Authentication required" });
+    if (!authHeader) {
+      return res.sendStatus(401);
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const token = authHeader.split(" ")[1] as string
+    if (!token) res.status(400)
+    const decoded = jwt.verify(token, 'process.env.JWT_SECRET' as  string);
     (req as any).user = decoded;
-    next();
+    console.log('decoded', decoded)
+    console.log(`${req.headers.authorization}`)
+    res.status(200).json({ msg: 'verifyed' })
+    // next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token" });
+    console.log('err', error)
+    return res.status(401).json({ message: "Invalid or expired token", err: error, er: `${req.headers.authorization}` });
   }
 };
--- checkout/index.ts
+++ checkout/index.ts
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     console.log('hello from checkout ')
     res.status(200).json({ msg: 'hi from checkout' })
 })
+app.get('/orders', (req, res) => {
+    console.log('hello from orders ')
+    res.status(200).json({ msg: 'hi from checkout' })
+})
 app.listen(3002, "0.0.0.0", () => {
     console.log('checkout')
 })
